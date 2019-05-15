101439ed4af1c6a92140b002fa118b48bb8e2b90
nradovanovic17 <burninginside13@gmail.com>
2019-05-15 14:35:30 +0200

    dodato da tip korisnika bude nullable

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('tip_korisnika');
+            $table->string('tip_korisnika')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
