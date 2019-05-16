1268e341568bb789e0e7c47eef4cd7a1fcb2e58e
AleksaCamilovic <aleksa.camilovic@outlook.com>
2019-05-16 13:24:53 +0200

    sredjenja login forma, izgleda cistije

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,14 +3,8 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-6">
             <div class="card">
-                <div class="card-header">
-                    <div>
-                       <img src="/storage/img/catering_logo.png" style="max-height: 100px; display: block; margin: auto;">
-                    </div>
-                </div>
-
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
@@ -57,12 +51,12 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Prijava') }}
+                                <button type="submit" class="btn btn-primary" style="width: 150px">
+                                    <b>{{ __('Prijava') }}</b>
                                 </button>
-
+                                <br>
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    <a class="btn btn-link" style="margin-top: 10px" href="{{ route('password.request') }}">
                                         {{ __('Zaboravio si lozinku?') }}
                                     </a>
                                 @endif
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-6 mt-3">
             <div class="card">
                 <div class="card-header">Dashboard</div>
 
@@ -18,6 +18,58 @@
                 </div>
             </div>
         </div>
+
+        <div class="col-md-6 mt-3">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    You are logged in!
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="row justify-content-center">
+        <div class="col-md-6 mt-3">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    You are logged in!
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6 mt-3">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    You are logged in!
+                </div>
+            </div>
+        </div>
+
     </div>
 </div>
 @endsection
