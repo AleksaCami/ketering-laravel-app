1d471a8f21ca4daa9baf59106e2a90fc0963847c
nradovanovic17 <burninginside13@gmail.com>
2019-06-03 14:06:55 +0200

    Sredjen prikaz proizvoda

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -7,30 +7,30 @@
             <h2>Prikaz proizvoda</h2>
             <br>
             <div class="table-responsive">
-                <table id="example" class="table table-striped table-bordered" style="width:100%">
+                <table id="example" class="table table-striped table-bordered">
                     <thead>
                     <tr>
-                        <th>Naziv<i style="margin-left: 10px" class="fas fa-arrows-alt-v"></i></th>
-                        <th>Jedinica mere<i style="margin-left: 10px" class="fas fa-arrows-alt-v"></i></th>
-                        <th>Cena<i style="margin-left: 10px" class="fas fa-arrows-alt-v"></i></th>
-                        <th>Opis<i style="margin-left: 10px" class="fas fa-arrows-alt-v"></i></th>
-                        <th>Kategorija<i style="margin-left: 10px" class="fas fa-arrows-alt-v"></i></th>
-                        <th>Slika</th>
-                        <th>Izmeni</th>
-                        <th>Obrisi</th>
+                        <th scope="col">Naziv<i style="margin-left: 10px" class="fas fa-arrows-alt-v"></i></th>
+                        <th scope="col" class="">Jedinica mere<i style="margin-left: 10px" class="fas fa-arrows-alt-v"></i></th>
+                        <th scope="col">Cena<i style="margin-left: 10px" class="fas fa-arrows-alt-v"></i></th>
+                        <th scope="col">Opis<i style="margin-left: 10px" class="fas fa-arrows-alt-v"></i></th>
+                        <th scope="col">Kategorija<i style="margin-left: 10px" class="fas fa-arrows-alt-v"></i></th>
+                        <th scope="col" style="width: 100px">Slika</th>
+                        <th scope="col">Izmeni</th>
+                        <th scope="col">Obrisi</th>
                     </tr>
                     </thead>
                     <tbody>
                     @foreach($products as $product)
                         <tr>
-                            <td>{{$product->naziv}}</td>
-                            <td>{{$product->mera}}</td>
-                            <td>{{$product->cena}}</td>
-                            <td>{{$product->opis}}</td>
-                            <td>{{$product->kuhinja->naziv}}</td>
-                            <td><img class="img-responsive" style="width: 100%" src="/storage/products_images/{{$product->products_images}}"></td>
-                            <td><a href="/products/edit/{{$product->id}}"><button class="btn btn-primary btn-xs"><i class="fas fa-edit"></i></button></a></td>
-                            <td>
+                            <td >{{$product->naziv}}</td>
+                            <td >{{$product->mera}}</td>
+                            <td >{{$product->cena}}</td>
+                            <td  >{{$product->opis}}</td>
+                            <td >{{$product->kuhinja->naziv}}</td>
+                            <td ><img class="img-fluid" style="height: auto" src="/storage/products_images/{{$product->products_images}}"></td>
+                            <td ><a href="/products/edit/{{$product->id}}"><button class="btn btn-primary btn-xs"><i class="fas fa-edit"></i></button></a></td>
+                            <td >
                                 <form method="POST" action="/products/destroy/{{$product->id}}">
                                     @csrf
                                     @method('DELETE')
